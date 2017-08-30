2733e6d72b336fc95758be62309c5d0c523e6ba4
Rémi Palancher <remi-externe.palancher@edf.fr>
2017-08-30 10:14:02 +0200

    Imported Debian patch 0.6.4-2~c9+4

--- debian/build/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-mk-build-deps -i -r -t 'apt-get -y' /opt/consul-deb/debian/control
-pushd /opt/consul-deb && gbp buildpackage $GIT_BUILDPACKAGE_OPTIONS
-cp /tmp/build-area/* /tmp/consul-deb
